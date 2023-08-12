ce1ef67dd2a83d69d7f9f3c50b05147105486f7a
Anuththiga <anuththiga25jt@gmail.com>
2023-08-12 21:24:20 +0800

    task container and header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/header.scss'
+
+function Header() {
+  return (
+    <div>
+      <input type='button' className='add-task' value="Add Task"/>
+    </div>
+  )
+}
+
+export default Header
