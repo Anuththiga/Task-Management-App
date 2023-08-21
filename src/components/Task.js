29a90633daedbe7c393239e5ce3a0a51d40ed626
Anuththiga <anuththiga25jt@gmail.com>
2023-08-21 22:09:34 +0800

    task and task status initial file

---
+++ src/components/Task.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+function Task() {
+  return (
+    <div>
+        <Button variant="info" size='sm'>&#171;</Button>
+        Task
+        <Button variant="info" size='sm'>&#187;</Button>
+    </div>
+  )
+}
+
+export default Task
